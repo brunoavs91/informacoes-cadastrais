9b09a45a30268530ce23299ae10d397a42133fa7
brunoav91@gmail.com <brunoav91@gmail.com>
2021-07-29 16:00:55 -0300

    iniciando projeto informacoes cadastrais

---
+++ src/test/java/com/informacoescadastrais/InformacoesCadastraisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informacoescadastrais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformacoesCadastraisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
